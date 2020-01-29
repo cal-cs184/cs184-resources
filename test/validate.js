ef2ff3ebe0ef4102be3eed2ab88fe78e32de113e
Zhuo Lu <sethlu@berkeley.edu>
2020-01-28 20:28:45 -0800

    Update validation script to check resource topic references

--- test/validate.js
+++ test/validate.js
@@ -10,7 +10,33 @@ const ajv = new Ajv({ allErrors: true });
 const schema = JSON.parse(fs.readFileSync(process.argv[3]));
 const data = yaml.safeLoad(fs.readFileSync(process.argv[2]));
 
+// Validate schema
+
 const validate = ajv.compile(schema);
 const valid = validate(data);
 
-if (!valid) console.log(validate.errors);
+if (!valid) {
+    console.log(validate.errors);
+    process.exit(1);
+}
+
+// Validate data-dependencies
+
+let num_bad_topics = 0;
+
+let resource_idx = 0;
+for (let resource of data.resources) {
+    let topic_idx = 0;
+    for (let topic of resource.topics) {
+        if (!data.topics[topic]) {
+            console.log(`resources[${resource_idx}].topics[${topic_idx}] "${topic}" not defined in topics list`);
+            num_bad_topics++;
+        }
+        topic_idx++;
+    }
+    resource_idx++;
+}
+
+if (num_bad_topics > 0) {
+    process.exit(1);
+}
